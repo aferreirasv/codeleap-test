385d013150cc9f9a0a977b18395ddea37a680056
Alan Ferreira <aferreirasv@gmail.com>
2023-07-24 21:16:16 -0300

    feat[router]: Adiciona router com rotas para pagina principal e pagina de login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SignUp from "./pages/SignUp/SignUp";
+import MainPage from "./pages/MainPage/MainPage";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
