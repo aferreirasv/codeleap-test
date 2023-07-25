18bde1f178e6c092e3e23f76c74b3ac23af06489
Alan Ferreira <aferreirasv@gmail.com>
2023-07-25 01:25:49 -0300

    feat[layout] Criacao de layout pagina de login do usuario

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#7696ed",
+      dark: "#6a87d5",
+      light: "#93a8e0",
+    },
+    secondary: {
+      main: "#DDDDDD",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,65 @@
+import "./SignUp.css";
+import { useState } from "react";
+import { Paper, TextField, Typography, Button } from "@mui/material";
+import { Navigate, useNavigate } from "react-router-dom";
+
 const SignUp = (props) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleUsername = (e) => {
+    setInputValue(e.currentTarget.value);
+  };
+
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    navigate("/");
+  };
   return (
     <>
-      <div>SignUp</div>
+      {false ? <Navigate to="/" /> : null /* Check if user is logged in*/}
+      <div className="SignUp">
+        <Paper sx={{ width: "90vw", maxWidth: "500px" }}>
+          <div className="SignUpCard">
+            <div className="SignUpHeader">
+              <Typography
+                variant="body1"
+                align="start"
+                sx={{ fontWeight: "800" }}
+              >
+                Welcome to CodeLeap network!
+              </Typography>
+            </div>
+            <div>
+              <Typography align="start">Please enter your username</Typography>
+              <TextField
+                placeholder="John Doe"
+                value={inputValue}
+                onChange={handleUsername}
+                fullWidth
+                size="small"
+              />
+            </div>
+            <div className="SignUpButtonContainer">
+              <div />
+              <Button
+                variant="contained"
+                disabled={!inputValue}
+                sx={{ right: "0px", width: "25%" }}
+                size="small"
+                onClick={handleSubmit}
+              >
+                <Typography
+                  variant="body"
+                  color={"white"}
+                  sx={{ fontWeight: "600" }}
+                >
+                  Enter
+                </Typography>
+              </Button>
+            </div>
+          </div>
+        </Paper>
+      </div>
     </>
   );
 };
